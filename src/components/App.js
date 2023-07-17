ca8024a5e497b0b70ca51d1a84a2cd5ce631d252
Amey <130839577+ameynagulwar@users.noreply.github.com>
2023-07-17 07:51:38 +0000

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React,{useState}from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [parent, setParent] = useState("");
+  let [child, setChild] = useState("")
   return (
     <div>
-        {/* Do not remove the main div */}
+         <div id='parent'style={{width: "100%", height:"100%", background:"green",color:"black"}}  onChange={() => setParent(child)}>
+        <h1>Parent component</h1>
+        <p>{parent}</p>
+         <div id='child' style={{width:"50%",height:"500px",backgroundColor:"brown", color:"black"}}>
+         <h3>Child component</h3>
+           <input type='text'style={{width:"200px",height:"100px", backgroundColor:"white", color:"black"}} onChange={(event) => setChild(event.target.value)}/>
+         </div>
+    </div>
     </div>
   )
 }
